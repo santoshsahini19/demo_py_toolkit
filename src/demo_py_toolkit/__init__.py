ab60ec83c79e1ae126fd1c3776ecf1ded4c22ce0
SanthoshSahini <santoshsahini@gmail.com>
2025-10-30 21:27:17 -0400

    package publish code

---
+++ src/demo_py_toolkit/__init__.py
@@ -0,0 +1,5 @@
+
+from .search import SearchPMCArticle
+from .download import DownloadPMCArticle
+
+__all__ = ['SearchPMCArticle', 'DownloadPMCArticle']
---
+++ src/demo_py_toolkit/cli.py
@@ -0,0 +1,27 @@
+# cli script for searching and downloading PMC articles
+
+import argparse
+from demo_py_toolkit.search import SearchPMCArticle
+from demo_py_toolkit.download import DownloadPMCArticle
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Basic PMC Article Downloader")
+
+    parser.add_argument("--query", type=str, required=True, help="Search query")
+    parser.add_argument("--field", type=str, default="Abstract",
+                        choices=["Title", "Abstract", "Text Word"])
+    parser.add_argument("--max_results", type=int, default=5, help="Maximum search results")
+    parser.add_argument("--output_directory", type=str, default="downloads")
+    parser.add_argument("--api_key", type=str, default="")
+
+    args = parser.parse_args()
+
+    searcher = SearchPMCArticle(api_key=args.api_key)
+    downloader = DownloadPMCArticle(api_key=args.api_key)
+
+    pmc_ids = searcher.search(args.query, args.field, args.max_results)
+    print(f"Found {len(pmc_ids)} articles for '{args.query}'")
+
+    downloader.download(pmc_ids, args.output_directory)
+    print("Download complete")
